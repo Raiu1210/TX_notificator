414930762598cb3ee7af4615970237afe0cf693b
Raiu1210 <baio1484@gmail.com>
2019-09-07 18:21:10 +0900

    trace block from now

--- monitor_block.js
+++ monitor_block.js
@@ -1,16 +1,28 @@
 const request = require('request');
 
-var options = {
+var counter = 0;
+var block_height = 0;
+
+var current_info = {
     url: 'https://blockbook.electrum-mona.org/api/',
     method: 'GET'
 }
 
 
-
-
 setInterval(function() {
-	request(options, function (error, response, body) {
-    	parsed_json = JSON.parse(body)
-    	console.log(parsed_json["blockbook"]["bestHeight"])
+	request(current_info, function (error, response, body) {
+		parsed_json = JSON.parse(body)
+		console.log("Now : " + parsed_json["blockbook"]["bestHeight"])
+		
+		if (counter == 0) {
+			counter = 1;
+		} else {
+			while (block_height < parseInt(parsed_json["blockbook"]["bestHeight"], 10)) {
+				console.log(block_height + 1)
+				block_height += 1
+			}
+		}
+	
+		block_height = parseInt(parsed_json["blockbook"]["bestHeight"], 10)
 	})
 }, 100000);
